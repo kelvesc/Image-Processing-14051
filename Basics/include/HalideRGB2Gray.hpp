f89de2259a8efb60f9e5e28353ea25d25419762f
Kelves Nunes da Costa <kelvesc@motorola.com>
2024-05-03 22:29:48 -0300

    build: adding include/ and registration/

---
+++ Basics/include/HalideRGB2Gray.hpp
@@ -0,0 +1,46 @@
+#ifndef HALIDE_RGB2GRAY
+#define HALIDE_RGB2GRAY
+
+#include "Halide.h"
+
+using namespace Halide;
+using namespace Halide::ConciseCasts;
+
+class HalideRGB2Gray : public Generator<HalideRGB2Gray> {
+    public:
+        Input<Func> img_input{"img_input", UInt(8), 3};
+        Output<Func> img_output{"img_output", Float(32), 1};
+
+        void generate() {
+            Expr scale = 1 / 255.f;
+            input_f32(x, y, c) = f32(img_input(x, y, c)) * scale;
+
+            Expr R = input_f32(x, y, 0);
+            Expr G = input_f32(x, y, 1);
+            Expr B = input_f32(x, y, 2);
+
+            Expr G = 0.299f * R + 0.587f * G + 0.114f * B
+
+            img_output(x, y, c) = G(img_input);
+        }
+
+        void schedule() {
+            if (using_autoscheduler()) {
+                img_input.set_estimates({{0, 1920}, {0, 1920}, {0, 3}});
+                img_output.set_estimates({{0, 1920}, {0, 1920}, {0, 3}});
+            } else {
+                // inline computation
+            }
+        }
+
+    private:
+        Var x{"x"}, y{"y"}, c{"c"};
+        Func input_f32{"input_f32"};
+        // Func compL{"compute_L"}, compa{"compute_a"}, compb{"compute_b"};
+
+        Expr f(Expr t) {
+            return select(t > 0.008856f, pow(t, 0.33333f), 7.787f * t + 0.137931f);
+        }
+};
+
+#endif
---
+++ Basics/src/registration/HalideRGB2Gray.cpp
@@ -0,0 +1,3 @@
+#include "HalideRGB2Gray.hpp"
+
+HALIDE_REGISTER_GENERATOR(HalideRGB2Gray, rgb2gray)
