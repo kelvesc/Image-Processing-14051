73e0e7c3649f63de447413339ade06e786ca16a2
Kelves <kelves.nunes@ufrpe.br>
2024-05-04 21:07:02 -0300

    fix: adjusted set_estimates and moved the convertion to grayscale to a Func

--- Basics/include/HalideRGB2Gray.hpp
+++ Basics/include/HalideRGB2Gray.hpp
@@ -11,7 +11,7 @@ class HalideRGB2Gray : public Generator<HalideRGB2Gray> {
 
         Input<Buffer<uint8_t>> img_input{"img_input_rgb", 3};
 
-        Output<Buffer<uint8_t>> img_output{"img_output_gray", 1};
+        Output<Buffer<uint8_t>> img_output{"img_output_gray", 3};
 
         void generate() {
             Expr scale = 1 / 255.0f;
@@ -21,15 +21,17 @@ class HalideRGB2Gray : public Generator<HalideRGB2Gray> {
             Expr G = input_f32(x, y, 1);
             Expr B = input_f32(x, y, 2);
 
-            Expr Gray = 0.299f * R + 0.587f * G + 0.114f * B;
+            // Expr Gray = 0.299f * R + 0.587f * G + 0.114f * B;
+            grayscale(x, y, c) = input_f32(x, y, .299f * R + 0.587f * G + 0.114f * B);
 
-            img_output(x, y, c) = cast<uint8_t>(Gray);
+            img_output(x, y, c) = u8(grayscale);
+            // img_output(x, y, c) = clamp(grayscale(x, y), 0, 1);
         }
 
         void schedule() {
             if (using_autoscheduler()) {
-                img_input.set_estimates({{0, 1920}, {0, 1920}, {0, 3}});
-                img_output.set_estimates({{0, 1920}, {0, 1920}, {0, 3}});
+                img_input.set_estimates({{0, 512}, {0, 512}});
+                img_output.set_estimates({{0, 512}, {0, 512}});
             } else {
                 // inline computation
             }
@@ -38,6 +40,7 @@ class HalideRGB2Gray : public Generator<HalideRGB2Gray> {
     private:
         Var x{"x"}, y{"y"}, c{"c"};
         Func input_f32{"input_f32"};
+        Func grayscale{"grayscale"};
 };
 
 #endif
