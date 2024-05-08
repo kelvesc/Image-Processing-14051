c076b40e33ae130ebfcee5cf716fcc66572d84f7
Kelves <kelves.nunes@ufrpe.br>
2024-05-07 23:49:49 -0300

    style: removed comments.

--- Basics/src/main.cpp
+++ Basics/src/main.cpp
@@ -23,11 +23,6 @@ int main(int argc, char**argv) {
     Buffer<uint8_t> img_input = load_image(path_in);
     Buffer<uint8_t> img_output = Buffer<uint8_t>::make_with_shape_of(img_input);
 
-    // rgb2gray = create<HalideRGB2Gray>();
-    // rgb2gray->apply(img_output_gray);
-    // img_output = u8(rgb2gray(img_input));
-
-
     rgb2gray(img_input, img_output);
 
     save_image(img_output, path_out);
